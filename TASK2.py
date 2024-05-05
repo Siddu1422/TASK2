34d5c4c9d5baf8310e84a03ade8f57705d7f73c5
Siddu1422 <102538935+Siddu1422@users.noreply.github.com>
2024-05-05 22:47:56 +0530

    Add files via upload

---
+++ TASK2.py
@@ -0,0 +1,38 @@
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 5)
+
+def check_winner(board, player):
+    # Check rows
+    for row in board:
+        if all(cell == player for cell in row):
+            return True
+
+    # Check columns
+    for col in range(3):
+        if all(board[row][col] == player for row in range(3)):
+            return True
+
+    # Check diagonals
+    if all(board[i][i] == player for i in range(3)) or \
+       all(board[i][2-i] == player for i in range(3)):
+        return True
+
+    return False
+
+def is_board_full(board):
+    return all(cell != ' ' for row in board for cell in row)
+
+def get_valid_move(board):
+    while True:
+        try:
+            move = int(input("Enter your move (1-9): ")) - 1
+            row, col = divmod(move, 3)
+            if 0 <= row < 3 and 0 <= col < 3 and board[row][col] == ' ':
+                return row, col
+            else:
+                print("Invalid move. Try again.")
+        except 
+
+ 
